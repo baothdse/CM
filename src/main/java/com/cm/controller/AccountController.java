94075eb51bbe81746eb6536509d80c63418378d6
VanNNT\Van <vannnt2112@gmail.com>
2017-02-16 09:20:39 +0700

    add ErrorConstant

--- src/main/java/com/cm/controller/AccountController.java
+++ src/main/java/com/cm/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cm.common.CustomeErrType;
+import com.cm.constants.ErrorConstants;
 import com.cm.constants.ParamConstants;
 import com.cm.constants.URLConstants;
 import com.cm.entity.AccountEntity;
@@ -29,11 +30,8 @@ public class AccountController {
 	public ResponseEntity<?> login(@RequestParam(value=ParamConstants.USERNAME) String username,
 			                       @RequestParam(value=ParamConstants.PASSWORD) String password){
 		AccountEntity profile = accountService.findByUserName(username);
-		if(profile==null){
-			ErrorCustome err = new ErrorCustome("403", "Wrong password or username");
-			return new ResponseEntity<ErrorCustome>(err, HttpStatus.OK);
-		}else if(!profile.getPassword().equals(password)){
-			ErrorCustome err = new ErrorCustome("403", "Wrong password or username");
+		if( profile==null || !profile.getPassword().equals(password)){
+			ErrorCustome err = new ErrorCustome(ErrorConstants.ER001, ErrorConstants.EM001);
 			return new ResponseEntity<ErrorCustome>(err, HttpStatus.OK);
 		}
 		return new ResponseEntity<AccountEntity>(profile, HttpStatus.OK); 
